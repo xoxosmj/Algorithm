ac2d60e115c0812f578c55db63fa205bbab373e5
xoxosmj <81194812+xoxosmj@users.noreply.github.com>
2025-01-24 22:00:12 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int slice, int n) {
+      for(int i=0;; i++){
+          if(i*slice >= n){
+              return i;
+              
+          }
+          
+      }
+    }
+}
