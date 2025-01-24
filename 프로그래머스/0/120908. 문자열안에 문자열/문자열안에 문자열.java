d1dd07b342de0dae94a5cf775ef30120e2b2fd67
xoxosmj <81194812+xoxosmj@users.noreply.github.com>
2025-01-24 22:20:21 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        if (str1.contains(str2)) return 1;
-        else return 2;
+        return str1.contains(str2) ? 1 : 2;
     }
 }
