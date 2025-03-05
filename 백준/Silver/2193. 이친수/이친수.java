63ff4be26c61e18a8c5884c68d86446dde2ec5f2
xoxosmj <81194812+xoxosmj@users.noreply.github.com>
2025-03-06 03:02:07 +0900

    [Silver III] Title: 이친수, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        if (n > 1) {
+            dp[2] = 1;
+        }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
+
+
+    }
+}
